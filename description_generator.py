ca03d54d0c77d5140ac15eb25ff03a9089b4d3fa
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2022-12-05 01:03:18 +0400

    work on OOP aspect of the code and moved hard coded identifier into config module

--- description_generator.py
+++ description_generator.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import config
 
 from dotenv import load_dotenv
 
@@ -10,19 +11,37 @@ if not OPENAI_SECRET_API_KEY:
     raise EnvironmentError('You should set OPENAI_SECRET_API_KEY as your environment variable.')
 
 
-def generate_description(keywords: list):
+class ProductDescription:
     openai.api_key = OPENAI_SECRET_API_KEY
-    model = openai.Completion.create(
-        engine='text-davinci-002',
-        prompt=f'Generate Product Description within Keywords\nKeywords: {keywords}\nProduct Description:',
-        max_tokens=80,
-        top_p=1.,
-        frequency_penalty=1.,
-        presence_penalty=1.,
-        n=2
-    )
 
-    return model
+    def __init__(self,
+                 engine=config.ENGINE,
+                 max_tokens=config.MAX_TOKENS,
+                 top_p=config.TOP_P,
+                 frequency_penalty=config.FREQUENCY_PENALTY,
+                 presence_penalty=config.PRESENCE_PENALTY):
+        self.engine = engine
+        self.max_tokens = max_tokens
+        self.top_p = top_p
+        self.frequency_penalty = frequency_penalty
+        self.presence_penalty = presence_penalty
+
+    def generate_description(self, keywords: list):
+        model = openai.Completion.create(
+            engine=self.engine,
+            prompt=f'Generate Product Description within Keywords\nKeywords: {keywords}\nProduct Description:',
+            max_tokens=self.max_tokens,
+            top_p=self.top_p,
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
+            n=2
+        )
+
+        first_description = model['choices'][0]['text']
+        second_description = model['choices'][1]['text']
+
+        return first_description, second_description
 
 
-print(generate_description(['Ucraft', 'e-commerce', 'website builder']))
+output = ProductDescription()
+print(output.generate_description(['Ucraft', 'e-commerce', 'website builder']))
