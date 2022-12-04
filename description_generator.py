5cfad4b67d4a4550d9c2e7bc171dc167cd5954e1
Norvik-Alexian <norvik.alexyan97@protonmail.com>
2022-12-04 14:43:37 +0400

    create a function that generates product description using keywords as input with GPT-3

--- description_generator.py
+++ description_generator.py
@@ -8,3 +8,21 @@ OPENAI_SECRET_API_KEY = os.getenv('OPENAI_SECRET_API_KEY')
 
 if not OPENAI_SECRET_API_KEY:
     raise EnvironmentError('You should set OPENAI_SECRET_API_KEY as your environment variable.')
+
+
+def generate_description(keywords: list):
+    openai.api_key = OPENAI_SECRET_API_KEY
+    model = openai.Completion.create(
+        engine='text-davinci-002',
+        prompt=f'Generate Product Description within Keywords\nKeywords: {keywords}\nProduct Description:',
+        max_tokens=80,
+        top_p=1.,
+        frequency_penalty=1.,
+        presence_penalty=1.,
+        n=2
+    )
+
+    return model
+
+
+print(generate_description(['Ucraft', 'e-commerce', 'website builder']))
